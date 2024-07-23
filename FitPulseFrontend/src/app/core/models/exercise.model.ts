5085f6e709aaa241b3025edc73dac2d39d8cea32
talida13 <talida.caraman@gmail.com>
2024-07-23 19:23:59 +0300

    schimbari de baza

---
+++ FitPulseFrontend/src/app/core/models/exercise.model.ts
@@ -0,0 +1,8 @@
+export interface Exercise  {
+    id: number;
+    name: string;
+    photo: string;
+    workout_id: number;
+    reps: number;
+    calories_rep: number;
+}
---
+++ FitPulseFrontend/src/app/core/models/workout.model.ts
@@ -0,0 +1,10 @@
+export interface Workout  {
+    id: number;
+    name: string;
+    photo: string;
+    author: string;
+    calories: number;
+    published_date: Date;
+    category: string;
+    difficulty: string;
+}
