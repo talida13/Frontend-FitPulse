516468772e65adc92a44251073e48d1c6709fc59
talida13 <talida.caraman@gmail.com>
2024-07-24 13:05:38 +0300

    design home page

--- FitPulseFrontend/src/app/features/features.module.ts
+++ FitPulseFrontend/src/app/features/features.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
+import { WorkoutsComponent } from './workouts/workouts.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginFormComponent,
+    RegisterFormComponent,
+    WorkoutsComponent
+  ],
   imports: [
     CommonModule
   ]
