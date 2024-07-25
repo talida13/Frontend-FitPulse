83c9d3d06e20d41af9d63c428291ecea9d971a58
talida13 <talida.caraman@gmail.com>
2024-07-25 12:48:22 +0300

    register- semi facut

--- FitPulseFrontend/src/app/features/register-form/register-form.component.ts
+++ FitPulseFrontend/src/app/features/register-form/register-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-form',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./register-form.component.scss']
 })
 export class RegisterFormComponent {
+  step: number = 1;
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      age: ['', Validators.required],
+      weight: ['', Validators.required],
+      height: ['', Validators.required],
+      gender: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    });
+  }
+
+  nextStep() {
+    if (this.step==1) {
+      this.step++;
+    }
+  }
+  //this.registerForm.valid
+
+  previousStep() {
+    this.step--;
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+      // Handle form submission, e.g., send data to the server
+    }
+  }
 
 }
