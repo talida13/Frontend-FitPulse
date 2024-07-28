3b572f7b922de4a603fb45fc5caa96b47a2f848a
talida13 <talida.caraman@gmail.com>
2024-07-29 00:58:00 +0300

    modificari workout

--- FitPulseFrontend/src/app/core/services/exercise.service.ts
+++ FitPulseFrontend/src/app/core/services/exercise.service.ts
@@ -8,7 +8,7 @@ export interface Exercise {
   id: number;
   name: string;
   photo: string;
-  workout_id: number;
+  workout_Id: number;
   reps: number;
   calories_rep: number;
 
--- FitPulseFrontend/src/app/layout/homepage/homepage.component.ts
+++ FitPulseFrontend/src/app/layout/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent {
-
+  constructor( private router: Router) { }
+  goToWorkouts()
+  {
+    this.router.navigate(['/Workouts']);
+  }
 }
