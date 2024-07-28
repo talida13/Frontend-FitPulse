8f886ad4293febd4ddb3e9dfeb687cf6b09ebf8c
FilipescuT <teo.filipescu@gmail.com>
2024-07-29 00:50:34 +0300

    admin gata

--- FitPulseFrontend/src/app/features/admin/admin.component.ts
+++ FitPulseFrontend/src/app/features/admin/admin.component.ts
@@ -1,4 +1,3 @@
-
 import { Component } from '@angular/core';
 
 @Component({
@@ -9,6 +8,17 @@ import { Component } from '@angular/core';
 export class AdminComponent {
   currentPage = 1;
   totalPages = 5; // Adjust this based on your requirement
+
+  selectedWorkout = '';
+  newWorkout = { name: '', calories: '', category: '', difficulty: '', exercises: '' };
+  selectedEditWorkout = '';
+  editWorkout = { exercises: '', category: '', difficulty: '', calories: '' };
+  exerciseToEdit = { name: '', sets: '', reps: '' };
+
+  workouts = ['treadmill workout', 'leg workout', 'back workout', 'running workout']; // List of workouts
+  categories = ['strength', 'cardio', 'flexibility'];
+  difficulties = ['easy', 'medium', 'hard'];
+  numberOfExercises = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   goToPage(page: number) {
     if (page > 0 && page <= this.totalPages) {
@@ -29,6 +39,24 @@ export class AdminComponent {
   }
 
   skip() {
-    // Implement skip logic if needed
+    this.goToNextPage();
+  }
+
+  removeWorkout() {
+    // Logic to remove workout
+    console.log('Workout removed:', this.selectedWorkout);
+    this.goToNextPage();
+  }
+
+  addWorkout() {
+    // Logic to add workout
+    console.log('Workout added:', this.newWorkout);
+    this.goToNextPage();
+  }
+
+  editExercise() {
+    // Logic to edit exercise
+    console.log('Exercise edited:', this.exerciseToEdit);
+    this.goToNextPage();
   }
 }
