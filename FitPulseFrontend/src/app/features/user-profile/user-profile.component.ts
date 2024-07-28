915354764f5ef307e42fc83865ecbe0d176edcdb
mateispic <1mateipricop@gmail.com>
2024-07-28 23:12:29 +0300

    User profile card - modificari

--- FitPulseFrontend/src/app/features/user-profile/user-profile.component.ts
+++ FitPulseFrontend/src/app/features/user-profile/user-profile.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
-import { Router } from '@angular/router';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.scss']
 })
+
 export class UserProfileComponent {
 
 }
