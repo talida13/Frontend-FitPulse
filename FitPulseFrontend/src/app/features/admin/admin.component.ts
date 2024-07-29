f8b398d1d0566d796efa0970c23f9d5a43424b58
FilipescuT <teo.filipescu@gmail.com>
2024-07-29 12:02:27 +0300

    admin incomplet

--- FitPulseFrontend/src/app/features/admin/admin.component.ts
+++ FitPulseFrontend/src/app/features/admin/admin.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+/*import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-admin',
@@ -57,6 +57,98 @@ export class AdminComponent {
   editExercise() {
     // Logic to edit exercise
     console.log('Exercise edited:', this.exerciseToEdit);
+    this.goToNextPage();
+  }
+}
+*/
+import { Component, OnInit } from '@angular/core';
+import { WorkoutService, Workout } from '../../core/services/workout.service';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.scss']
+})
+export class AdminComponent implements OnInit {
+  currentPage = 1;
+  totalPages = 5; // Ajustează după necesități
+
+  workouts: Workout[] = [];
+  newWorkout: Workout = { id: 0, name: '', photo: '', author: '', published_Date: new Date(), category: '', difficulty: '' };
+  selectedWorkoutId: number | null = null;
+  editWorkoutData: Workout | null = null;
+
+  constructor(private workoutService: WorkoutService) {}
+
+  ngOnInit(): void {
+    this.loadWorkouts();
+  }
+
+  loadWorkouts(): void {
+    this.workoutService.getWorkouts().subscribe(
+      (data: Workout[]) => this.workouts = data,
+      (error) => console.error(error)
+    );
+  }
+
+  goToPage(page: number) {
+    if (page > 0 && page <= this.totalPages) {
+      this.currentPage = page;
+    }
+  }
+
+  goToPreviousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
+  }
+
+  goToNextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+    }
+  }
+
+  addWorkout() {
+    this.workoutService.addWorkout(this.newWorkout).subscribe(
+      () => {
+        this.loadWorkouts();
+        this.goToNextPage();
+      },
+      (error) => console.error(error)
+    );
+  }
+
+  deleteWorkout() {
+    if (this.selectedWorkoutId !== null) {
+      this.workoutService.deleteWorkout(this.selectedWorkoutId).subscribe(
+        () => {
+          this.loadWorkouts();
+          this.goToNextPage();
+        },
+        (error) => console.error(error)
+      );
+    }
+  }
+
+  selectWorkout(id: number) {
+    this.selectedWorkoutId = id;
+    this.editWorkoutData = this.workouts.find(workout => workout.id === id) || null;
+  }
+
+  updateWorkout() {
+    if (this.editWorkoutData !== null) {
+      this.workoutService.updateWorkout(this.editWorkoutData).subscribe(
+        () => {
+          this.loadWorkouts();
+          this.goToNextPage();
+        },
+        (error) => console.error(error)
+      );
+    }
+  }
+
+  skip() {
     this.goToNextPage();
   }
 }
